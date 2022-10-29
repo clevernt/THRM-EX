9ca34c360ff82e60923a674f3aab140e701a2e1f
clevershadowx <azozy2011.hd@gmail.com>
2022-10-29 17:17:32 +0300

    a

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 import lightbulb
 from dotenv import load_dotenv
 import os
+import miru
 load_dotenv()
 
 bot = lightbulb.BotApp(
     token=os.getenv('token'),
     default_enabled_guilds=753858152486797352
 )
+miru.load(bot)
 
 bot.load_extensions_from('./extensions')
 
