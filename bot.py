51590e14aa26a4d2ca43b3a7e893964538e6410e
clevershadowx <azozy2011.hd@gmail.com>
2022-11-09 06:23:39 +0300

    nvm

--- bot.py
+++ bot.py
@@ -4,8 +4,7 @@ import os
 load_dotenv()
 
 bot = lightbulb.BotApp(
-    token=os.getenv('token'),
-    default_enabled_guilds=753858152486797352
+    token=os.getenv('token')
 )
 
 bot.load_extensions_from('./extensions')
