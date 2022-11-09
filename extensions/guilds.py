9f4986f5e993ae4737884bdb8142e02e2c572af8
clevershadowx <azozy2011.hd@gmail.com>
2022-11-09 06:18:49 +0300

    t

---
+++ extensions/guilds.py
@@ -0,0 +1,15 @@
+import lightbulb
+from lightbulb.utils.pag import StringPaginator
+
+plugin = lightbulb.Plugin('guilds')
+
+@plugin.command()
+@lightbulb.command('guilds', 'List of Guilds the bot is in.')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def guilds(ctx):
+    guilds = await plugin.rest.fetch_my_guilds()
+    pag = StringPaginator(max_lines=10)
+    for n, guild in enumerate(guilds, start=1):
+        pag.add_line(f"**{n}.** {guild.name}")
+    for page in pag.build_pages():
+        await ctx.respond(page)
