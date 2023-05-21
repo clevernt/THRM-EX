7da32bff77e13fd6c5c01540668fe50597b8f78b
Clever <Azozy2011.hd@gmail.com>
2023-05-21 21:22:38 +0300

    sdfsdfsd

--- extensions/modulebeta.py
+++ extensions/modulebeta.py
@@ -38,14 +38,24 @@ async def modulebeta(ctx):
     operator_data = OPERATORS[operator_name]
     module_names = [module_data["module_branch"]
                     for module_data in operator_data]
+    print(f"Module names: {module_names}")
     module_options = [hikari.SelectOption(
         module_name, module_name) for module_name in module_names]
     components = [hikari.SelectMenu(
         "module_select", "Select the module to display", options=module_options)]
     message = await ctx.respond(components=components)
-    interaction = await message.wait_for(hikari.SelectOptionInteraction)
+    try:
+        interaction = await message.wait_for(hikari.SelectOptionInteraction)
+    except TimeoutError:
+        await ctx.edit_initial_response(":x: Timed out.", components=None)
+        return
+    print(f"Selected module: {interaction.selected_option.value}")
     module_data = [module for module in operator_data if module["module_branch"]
-                   == interaction.selected_option.value][0]
+                   == interaction.selected_option.value]
+    if len(module_data) == 0:
+        await ctx.edit_initial_response(":x: Module not found.", components=None)
+        return
+    module_data = module_data[0]
     module_embed = create_embed(module_data)
     await interaction.edit_initial_response(embed=module_embed)
 
