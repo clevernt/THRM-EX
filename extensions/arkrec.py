37a5c990de6d19587723c5c14ff8ebfa15167481
Clever <Azozy2011.hd@gmail.com>
2023-08-11 22:07:31 +0300

    test

--- extensions/arkrec.py
+++ extensions/arkrec.py
@@ -1,17 +1,42 @@
-from typing import Union
-from typing import Sequence
-from thefuzz import fuzz
-from thefuzz import process
+from typing import Union, Sequence
+from typing import Optional
+from difflib import get_close_matches
+from requests_html import HTMLSession
+import re
 import lightbulb
 import hikari
 import requests
 import json
-from difflib import get_close_matches
-from requests_html import HTMLSession
-import re
 
 session = HTMLSession()
 plugin = lightbulb.Plugin('arkrec')
+
+STAGE_TABLE_PATH = "./data/stage_table.json"
+OPS_NAMES_PATH = "./data/ops_names.json"
+CATEGORIES_PATH = "./data/categories.json"
+
+
+def load_json(file_path):
+    with open(file_path, encoding="utf-8") as f:
+        return json.load(f)
+
+
+def create_embed(title, fields, thumbnail_url=None):
+    embed = hikari.Embed(title=title)
+    for name, value, inline in fields:
+        embed.add_field(name=name, value=value, inline=inline)
+    if thumbnail_url:
+        embed.set_image(url=thumbnail_url)
+    return embed
+
+
+def find_image_url(urls):
+    for url in urls:
+        if re.match(r'(?:([^:/?#]+):)?(?://([^/?#]*))?([^?#]*\.(?:jpg|gif|png))(?:\?([^#]*))?(?:#(.*))?', url):
+            if "800px" in url:
+                return url
+    return None
+
 
 @plugin.command
 @lightbulb.option('category', 'Category', required=True, autocomplete=True)
@@ -20,163 +45,132 @@ plugin = lightbulb.Plugin('arkrec')
 @lightbulb.command('arkrec', 'finds clears from arkrec', auto_defer=True)
 @lightbulb.implements(lightbulb.SlashCommand)
 async def arkrec(ctx):
+    stages_data = load_json(STAGE_TABLE_PATH)
+    ops_data = load_json(OPS_NAMES_PATH)
+    categories_data = load_json(CATEGORIES_PATH)
+
     arkrec_url = "https://arkrec.com/api/records"
     stage = ctx.options.stage.strip()
-    categoryy = ctx.options.category.strip()
-    if ctx.options.mode:
-        mode = ctx.options.mode.strip()
-        if mode == "Normal Mode":
-            mode = "normal"
-        elif mode == "Challenge Mode":
-            mode = "challenge"
-        else:
-            mode = None
-    else:
-        mode = None
-    with open("./data/stage_table.json", encoding="utf-8") as f:
-        stagesdata = json.load(f)
-        stages = stagesdata["stages"]
-        for key, stage_value in stages.items():
-            if stage_value["code"].lower() == stage.lower():
-                stage_name = stage_value["name"]
-    try:
-        payload = {
-            "operation": stage.upper(),
-            "cn_name": stage_name
-        }
-    except UnboundLocalError:
-        await ctx.respond(f"{ctx.author.mention} Are you sure that's a stage?",)
+    category = ctx.options.category.strip()
+    mode = ctx.options.mode.strip() if ctx.options.mode else None
+
+    stage_value = stages_data["stages"].get(stage.lower())
+    if not stage_value:
+        await ctx.respond(f"{ctx.author.mention} Are you sure that's a valid stage?")
+        return
+
+    stage_name = stage_value["name"]
+    operation_type = stage_value.get("operationType")
+
+    payload = {
+        "operation": stage.upper(),
+        "cn_name": stage_name
+    }
+
     headers = {
         "Content-Type": "application/json",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0"
     }
-    response = requests.request("POST", arkrec_url, json=payload, headers=headers)
+
+    response = requests.request(
+        "POST", arkrec_url, json=payload, headers=headers)
     data = response.json()
-    with open("./data/ops_names.json", encoding="utf-8") as c:
-        opsdata = json.load(c)
-        Ops = opsdata["Operators"]
 
-    with open("./data/categories.json", encoding="utf-8") as g:
-        categoriesdata = json.load(g)
-        Categories = categoriesdata["Categories"]
+    categories_en = [value.lower()
+                     for value in categories_data["Categories"].values()]
 
-    clear_found = []
-    for i in data:
+    found_clear = False
+    for clear_data in data:
         try:
-            operationType = i["operationType"]
+            operation_type = clear_data["operationType"]
         except KeyError:
-            operationType = None
+            operation_type = None
             if mode == "challenge":
                 await ctx.respond(f"{ctx.author.mention} This stage does not have CM.")
                 break
-        else:
-            pass
-        category = i["category"]
-        categories_en = []
-        for x in category:
-            for key, value in Categories.items():
-                if key == x:
-                    en_name = value
-                    categories_en.append(en_name)
-        if (categoryy.title() in categories_en) and (operationType == mode or mode == None):
-            clear_found.append(True)
-            stage = i["operation"]
-            clear_link = i["url"]
-            ops_count = len(i["team"])
-            ops = i["team"]
-            raider = i["raider"]
-            date = i["date_published"][:10]
+
+        if (category in categories_en) and (operation_type == mode or not mode):
+            found_clear = True
+            stage = clear_data["operation"]
+            clear_link = clear_data["url"]
+            ops_count = len(clear_data["team"])
+            ops = clear_data["team"]
+            raider = clear_data["raider"]
+            date = clear_data["date_published"][:10]
             ops_en_ver = []
-            for i in ops:
-                if i in ["黑角", "夜刀", "巡林者", "杜林"]:
-                    for key, value in Ops.items():
-                        if value == i:
-                            en_name = key
-                            ops_en_ver.append(f"{en_name}")
-                elif i in ["Lancet-2", "Castle-3", "THRM-EX"]:
-                    ops_en_ver.append(i)
-                elif i == "正义骑士号":
+
+            for op in ops:
+                if op in ops_data["Operators"]:
+                    ops_en_ver.append(ops_data["Operators"][op])
+                elif op in ["Lancet-2", "Castle-3", "THRM-EX"]:
+                    ops_en_ver.append(op)
+                elif op == "正义骑士号":
                     ops_en_ver.append("Justice Knight")
                 else:
-                    skill = i[-1]
-                    cn_name = i.rstrip(i[-1])
-                    for key, value in Ops.items():
-                        if value == cn_name:
-                            en_name = key
-                            ops_en_ver.append(f"{en_name} S{skill}")
+                    skill = op[-1]
+                    cn_name = op.rstrip(skill)
+                    if cn_name in ops_data["Operators"]:
+                        ops_en_ver.append(
+                            f"{ops_data['Operators'][cn_name]} S{skill}")
+
             sep = ", "
-            #await ctx.respond(f"Stage: {stage}, Category(s): {sep.join(map(str, categories_en))}, Lowest ops: {ops_count}, Squad: {sep.join(map(str, ops_en_ver))} Link: {clear_link}")
-            embed = hikari.Embed(title="Clear Found")
-            embed.add_field("Stage", stage, inline=True)
+
             adverse_stages = {"10-7", "10-11", "10-15", "10-17",
-                            "11-3", "11-8", "11-12", "11-15",
-                            "11-20", "12-7", "12-13", "12-19", "12-20"}
+                              "11-3", "11-8", "11-12", "11-15",
+                              "11-20", "12-7", "12-13", "12-19", "12-20"}
 
             if stage in adverse_stages:
-                if operationType == "challenge":
-                    stage_url = f"https://prts.wiki/w/文件:磨难{stage.upper()}_{stage_name}_地图.png"
-                else:
-                    stage_url = f"https://prts.wiki/w/文件:{stage.upper()}_{stage_name}_地图.png"
+                stage_url = f"https://prts.wiki/w/文件:磨难{stage.upper()}_{stage_name}_地图.png" if operation_type == "challenge" else f"https://prts.wiki/w/文件:{stage.upper()}_{stage_name}_地图.png"
             else:
                 stage_url = f"https://prts.wiki/w/文件:{stage.upper()}_{stage_name}_地图.png"
 
             resp = session.get(stage_url)
             urls = resp.html.absolute_links
+            stage_thumbnail_url = find_image_url(urls)
 
-            found_thumbnail_url = False
-            for url in urls:
-                if re.match(r'(?:([^:/?#]+):)?(?://([^/?#]*))?([^?#]*\.(?:jpg|gif|png))(?:\?([^#]*))?(?:#(.*))?', url):
-                    if "800px" in url:
-                        stage_thumbnail_url = url
-                        found_thumbnail_url = True
-                        break
-            if not found_thumbnail_url:
-                stage_thumbnail_url = None
-            if operationType == "challenge":
-                embed.add_field("CM", "✅", inline=True)
-            elif operationType == "normal":
-                embed.add_field("CM", "❌", inline=True)
-            else:
-                pass
-            embed.add_field("Player", raider, inline=True)
-            embed.add_field("Category(s)", sep.join(map(str, categories_en)), inline=True)
-            embed.add_field("Operator Count", ops_count, inline=True)
-            embed.add_field("Squad", sep.join(map(str, ops_en_ver)), inline=True)
-            embed.add_field("Date", date, inline=True)
-            embed.add_field("Link", clear_link, inline=True)
-            embed.set_image(stage_thumbnail_url)
-            await ctx.respond(embed)
+            cm_field = "✅" if operation_type == "challenge" else "❌"
+
+            fields = [
+                ("Stage", stage, True),
+                ("CM", cm_field, True),
+                ("Player", raider, True),
+                ("Category(s)", sep.join(map(str, categories_en)), True),
+                ("Operator Count", ops_count, True),
+                ("Squad", sep.join(map(str, ops_en_ver)), True),
+                ("Date", date, True),
+                ("Link", clear_link, True)
+            ]
+
+            embed = create_embed("Clear Found", fields, stage_thumbnail_url)
+            await ctx.respond(embed=embed)
             break
-        else:
-            clear_found.append(False)
-    if True not in clear_found:
+
+    if not found_clear:
         await ctx.respond(f"{ctx.author.mention} Couldn't find a clear", flags=hikari.MessageFlag.EPHEMERAL)
+
 
 @arkrec.autocomplete("category")
 async def arkrec_autocomplete(
     opt: hikari.AutocompleteInteractionOption, inter: hikari.AutocompleteInteraction
-    ) -> Union[str, Sequence[str], hikari.CommandChoice, Sequence[hikari.CommandChoice]]:
+) -> Union[str, Sequence[str], hikari.CommandChoice, Sequence[hikari.CommandChoice]]:
     user_input = opt.value
-    with open("./data/categories.json", encoding="utf-8") as g:
-        categoriesdata = json.load(g)
-        Categories = categoriesdata["Categories"]  
-        categories_en = []
-        for _, value in Categories.items():
-            en_name = value.lower()
-            categories_en.append(en_name)
+    categories_data = load_json(CATEGORIES_PATH)
+    categories_en = [value.lower()
+                     for value in categories_data["Categories"].values()]
     close_matches = get_close_matches(user_input, categories_en, cutoff=0.1)
     return close_matches
+
 
 @plugin.command
 @lightbulb.command('categories', 'Lists all arkrec categories')
 @lightbulb.implements(lightbulb.SlashCommand, lightbulb.PrefixCommand)
 async def categories(ctx):
-    with open("./data/categories.json", encoding="utf-8") as f:
-        categoriesdata = json.load(f)
-        categories = categoriesdata["Categories"]
+    categories_data = load_json(CATEGORIES_PATH)
+    categories = categories_data["Categories"]
 
     embed = hikari.Embed()
-    embed.add_field("Categories:", "\n".join([f"• {v}" for v in categories.values()]))
+    embed.add_field("Categories:", "\n".join(
+        [f"• {v}" for v in categories.values()]))
     await ctx.respond(embed=embed, flags=hikari.MessageFlag.EPHEMERAL)
 
 
