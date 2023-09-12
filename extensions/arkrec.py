2c3eadcb9b5b782ac34554e6de5e40efbaeffaa4
Clever <Azozy2011.hd@gmail.com>
2023-09-12 21:17:50 +0300

    A lot.

--- extensions/arkrec.py
+++ extensions/arkrec.py
@@ -1,67 +1,120 @@
-from typing import Union
-from typing import Sequence
-from thefuzz import fuzz
-from thefuzz import process
 import lightbulb
 import hikari
 import requests
 import json
-from difflib import get_close_matches
-from requests_html import HTMLSession
 import re
 
+from typing import Union
+from typing import Sequence
+from difflib import get_close_matches
+from requests_html import HTMLSession
+
 session = HTMLSession()
-plugin = lightbulb.Plugin('arkrec')
+plugin = lightbulb.Plugin("arkrec")
+
+ARKREC_URL = "https://arkrec.com/api/records"
+
+mode_mapping = {"Normal Mode": "normal", "Challenge Mode": "challenge"}
+adverse_stages = {
+    "10-7",
+    "10-11",
+    "10-15",
+    "10-17",
+    "11-3",
+    "11-8",
+    "11-12",
+    "11-15",
+    "11-20",
+    "12-7",
+    "12-13",
+    "12-19",
+    "12-20",
+}
+skilless_operators = [
+    "Friston-3",
+    "U-Official",
+    "THRM-EX",
+    "泰拉大陆调查团",
+    "Lancet-2",
+    "Castle-3",
+    "正义骑士号",
+]
+
+with open("./ArknightsGameData/zh_CN/excel/stage_table.json", encoding="utf-8") as f:
+    stage_table = json.load(f)["stages"]
+with open("./data/operator_names.json", encoding="utf-8") as f:
+    operator_names = json.load(f)["operators"]
+with open("./data/categories.json", encoding="utf-8") as f:
+    categories = json.load(f)["categories"]
+
+
+def filter_and_translate(operators_list, operator_names):
+    operators_list_en = []
+    for operator in operators_list:
+        if operator in skilless_operators:
+            operators_list_en.append(operator_names[operator])
+        else:
+            skill = operator[-1]
+            name_cn = operator.rstrip(skill)
+            name_en = operator_names[name_cn]
+            operators_list_en.append(f"{name_en} S{skill}")
+    return operators_list_en
 
 
 @plugin.command
-@lightbulb.option('category', 'Category', required=True, autocomplete=True)
-@lightbulb.option('mode', "Mode", choices=["Normal Mode", "Challenge Mode"], required=False, default=None)
-@lightbulb.option('stage', 'Stage Name', required=True)
-@lightbulb.command('arkrec', 'finds clears from arkrec', auto_defer=True)
+@lightbulb.option(
+    "category", 
+    "Category",
+    required=True, 
+    autocomplete=True
+)
+@lightbulb.option(
+    "mode",
+    "Mode",
+    choices=["Normal Mode", "Challenge Mode"],
+    required=False,
+    default=None,
+)
+@lightbulb.option(
+    "stage", 
+    "Stage Name", 
+    required=True
+)
+@lightbulb.command(
+    "arkrec", 
+    "finds clears from arkrec", 
+    auto_defer=True
+)
 @lightbulb.implements(lightbulb.SlashCommand)
 async def arkrec(ctx):
-    arkrec_url = "https://arkrec.com/api/records"
-    stage = ctx.options.stage.strip()
-    categoryy = ctx.options.category.strip()
-    if ctx.options.mode:
-        mode = ctx.options.mode.strip()
-        if mode == "Normal Mode":
-            mode = "normal"
-        elif mode == "Challenge Mode":
-            mode = "challenge"
-        else:
-            mode = None
+    requested_stage = ctx.options.stage.strip()
+    requested_category = ctx.options.category.strip()
+    mode = mode_mapping.get(ctx.options.mode.strip(), None)
+
+    for _, value in stage_table.items():
+        if value["code"].lower() == requested_stage.lower():
+            stage_name = value["name"]
+            break
+
+    if stage_name is not None:
+        payload = {"operation": requested_stage.upper(), "cn_name": stage_name}
     else:
-        mode = None
-    with open("./data/stage_table.json", encoding="utf-8") as f:
-        stagesdata = json.load(f)
-        stages = stagesdata["stages"]
-        for key, stage_value in stages.items():
-            if stage_value["code"].lower() == stage.lower():
-                stage_name = stage_value["name"]
-    try:
-        payload = {
-            "operation": stage.upper(),
-            "cn_name": stage_name
-        }
-    except UnboundLocalError:
-        await ctx.respond(f"{ctx.author.mention} Are you sure that's a stage?",)
+        await ctx.respond(
+            hikari.Embed(description=f"Stage `{requested_stage}` was not found")
+        )
+
     headers = {
         "Content-Type": "application/json",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0",
     }
-    response = requests.request(
-        "POST", arkrec_url, json=payload, headers=headers)
+
+    try:
+        response = requests.request("POST", ARKREC_URL, json=payload, headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        await ctx.respond(hikari.Embed(description=f"Request error: {e}"))
+        return  
     data = response.json()
-    with open("./data/ops_names.json", encoding="utf-8") as c:
-        opsdata = json.load(c)
-        Ops = opsdata["Operators"]
-
-    with open("./data/categories.json", encoding="utf-8") as g:
-        categoriesdata = json.load(g)
-        Categories = categoriesdata["Categories"]
-
     clear_found = []
     for i in data:
         try:
@@ -69,92 +122,62 @@ async def arkrec(ctx):
         except KeyError:
             operationType = None
             if mode == "challenge":
-                await ctx.respond(f"{ctx.author.mention} This stage does not have CM.")
+                await ctx.respond(
+                    hikari.Embed(
+                        description=f"`{requested_stage}` does not have Challenge Mode"
+                    )
+                )
                 break
         else:
             pass
-        category = i["category"]
-        categories_en = []
-        for x in category:
-            for key, value in Categories.items():
-                if key == x:
-                    en_name = value
-                    categories_en.append(en_name)
-        if (categoryy.title() in categories_en) and (operationType == mode or mode == None):
+
+        categories_cn = i["category"]
+        categories_en = [categories.get(category) for category in categories_cn]
+
+        if (
+            requested_category.title() in categories_en
+            and operationType == mode
+            or mode == None
+        ):
             clear_found.append(True)
-            stage = i["operation"]
+            stage_code = i["operation"]
             clear_link = i["url"]
-            ops_count = len(i["team"])
-            ops = i["team"]
+            operator_count = len(i["team"])
+            operators_list = i["team"]
             raider = i["raider"]
-            date = i["date_published"][:10]
-            ops_en_ver = []
-            for i in ops:
-                if i in ["黑角", "夜刀", "巡林者", "杜林"]:
-                    for key, value in Ops.items():
-                        if value == i:
-                            en_name = key
-                            ops_en_ver.append(f"{en_name}")
-                elif i in ["Lancet-2", "Castle-3", "THRM-EX"]:
-                    ops_en_ver.append(i)
-                elif i == "正义骑士号":
-                    ops_en_ver.append("Justice Knight")
-                else:
-                    skill = i[-1]
-                    cn_name = i.rstrip(i[-1])
-                    for key, value in Ops.items():
-                        if value == cn_name:
-                            en_name = key
-                            ops_en_ver.append(f"{en_name} S{skill}")
+            date_published = i["date_published"][:10]
+            operators_list_en = filter_and_translate(operators_list, operator_names)
+
             sep = ", "
-            # await ctx.respond(f"Stage: {stage}, Category(s): {sep.join(map(str, categories_en))}, Lowest ops: {ops_count}, Squad: {sep.join(map(str, ops_en_ver))} Link: {clear_link}")
             embed = hikari.Embed(title="Clear Found")
-            embed.add_field("Stage", stage, inline=True)
-            adverse_stages = {"10-7", "10-11", "10-15", "10-17",
-                              "11-3", "11-8", "11-12", "11-15",
-                              "11-20", "12-7", "12-13", "12-19", "12-20"}
+            embed.add_field("Stage", stage_code, inline=True)
 
-            if stage in adverse_stages:
-                if operationType == "challenge":
-                    stage_url = f"https://prts.wiki/w/文件:磨难{stage.upper()}_{stage_name}_地图.png"
-                else:
-                    stage_url = f"https://prts.wiki/w/文件:{stage.upper()}_{stage_name}_地图.png"
-            else:
-                stage_url = f"https://prts.wiki/w/文件:{stage.upper()}_{stage_name}_地图.png"
 
-            resp = session.get(stage_url)
-            urls = resp.html.absolute_links
-
-            found_thumbnail_url = False
-            for url in urls:
-                if re.match(r'(?:([^:/?#]+):)?(?://([^/?#]*))?([^?#]*\.(?:jpg|gif|png))(?:\?([^#]*))?(?:#(.*))?', url):
-                    if "800px" in url:
-                        stage_thumbnail_url = url
-                        found_thumbnail_url = True
-                        break
-            if not found_thumbnail_url:
-                stage_thumbnail_url = None
             if operationType == "challenge":
                 embed.add_field("CM", "✅", inline=True)
             elif operationType == "normal":
                 embed.add_field("CM", "❌", inline=True)
             else:
                 pass
+
             embed.add_field("Player", raider, inline=True)
-            embed.add_field("Category(s)", sep.join(
-                map(str, categories_en)), inline=True)
-            embed.add_field("Operator Count", ops_count, inline=True)
-            embed.add_field("Squad", sep.join(
-                map(str, ops_en_ver)), inline=True)
-            embed.add_field("Date", date, inline=True)
+            embed.add_field(
+                "Category(s)", sep.join(map(str, categories_en)), inline=True
+            )
+            embed.add_field("Operator Count", operator_count, inline=True)
+            embed.add_field("Squad", sep.join(map(str, operators_list_en)), inline=True)
+            embed.add_field("Date Published", date_published, inline=True)
             embed.add_field("Link", clear_link, inline=True)
-            embed.set_image(stage_thumbnail_url)
             await ctx.respond(embed)
             break
         else:
             clear_found.append(False)
     if True not in clear_found:
-        await ctx.respond(f"{ctx.author.mention} Couldn't find a clear", flags=hikari.MessageFlag.EPHEMERAL)
+        await ctx.respond(
+            hikari.Embed(
+                description=f"No clear found matching the following input:\nStage: `{requested_stage}\nCategory: `{requested_category}`"
+            )
+        )
 
 
 @arkrec.autocomplete("category")
@@ -174,7 +197,7 @@ async def arkrec_autocomplete(
 
 
 @plugin.command
-@lightbulb.command('categories', 'Lists all arkrec categories')
+@lightbulb.command("categories", "Lists all arkrec categories")
 @lightbulb.implements(lightbulb.SlashCommand, lightbulb.PrefixCommand)
 async def categories(ctx):
     with open("./data/categories.json", encoding="utf-8") as f:
@@ -182,8 +205,7 @@ async def categories(ctx):
         categories = categoriesdata["Categories"]
 
     embed = hikari.Embed()
-    embed.add_field("Categories:", "\n".join(
-        [f"• {v}" for v in categories.values()]))
+    embed.add_field("Categories:", "\n".join([f"• {v}" for v in categories.values()]))
     await ctx.respond(embed=embed, flags=hikari.MessageFlag.EPHEMERAL)
 
 
--- extensions/modules.py
+++ extensions/modules.py
@@ -1,79 +1,84 @@
-from difflib import get_close_matches
-from typing import Sequence, Union
-
 import lightbulb
 import hikari
 import json
 
+from difflib import get_close_matches
+from typing import Sequence, Union
+
 bot = lightbulb.BotApp
 plugin = lightbulb.Plugin('modules')
 
+operators_with_modules = set()
+with open("./data/modules.json", "r") as f:
+    data = json.load(f)
+    for module in data:
+        operator_name = module["operator"].lower()
+        operators_with_modules.add(operator_name)
+    
+def get_operator_avatar(operator_name):
+    with open("./data/operator_ids.json", "r") as f:
+        data = json.load(f)
+        for operator, _id in data.items():
+            if operator == operator_name.title():
+                operator_id = _id
+    avatar_url = f"https://raw.githubusercontent.com/Aceship/Arknight-Images/main/avatars/{operator_id}.png"
+    return avatar_url
 
 @plugin.command
-@lightbulb.option('operator', 'Operator', required=True, autocomplete=True)
-@lightbulb.command('module', "Get details about an operator's module", auto_defer=True)
+@lightbulb.option(
+    'operator', 
+    'Operator', 
+    required=True, 
+    autocomplete=True
+)
+@lightbulb.command(
+    'module', 
+    "Get details about an operator's module", 
+    auto_defer=True
+)
 @lightbulb.implements(lightbulb.SlashCommand, lightbulb.PrefixCommand)
 async def module(ctx):
-    operator = ctx.options.operator.strip()
-    with open("./data/modules.json", "r") as f:
-        ops_with_modules = []
-        operators_list = []
-        data = json.load(f)
-        for i in data:
-            op_name = i["operator"]
-            ops_with_modules.append(op_name.lower())
-    with open("./data/operator_name_to_id.json", "r") as c:
-        ops_id_data = json.load(c)
-        for key, _ in ops_id_data.items():
-            operators_list.append(key.lower())
-        if operator.lower() not in ops_with_modules:
-            if operator.lower() in operators_list:
-                await ctx.respond(f"{operator.title()} does not have a module *yet*", flags=hikari.MessageFlag.EPHEMERAL)
+    requested_operator = ctx.options.operator.strip()
+    if requested_operator.lower() not in operators_with_modules:
+        await ctx.respond(
+            hikari.Embed(
+                description=f"{requested_operator} is either an invalid operator or does not have a module yet"
+                )
+            )
+    avatar_url = get_operator_avatar(requested_operator)
+    embeds = []
+    for i in data:
+        operator = i["operator"]
+        if requested_operator.lower() == operator.lower():
+            module_branch = i["module_branch"]
+            stage_1_trait_upgrade = i["stage_1_trait_upgrade"]
+            base_talent = i["base_talent"]
+            stage_2_talent_upgrade = i["stage_2_talent_upgrade"]
+            stage_3_talent_upgrade = i["stage_3_talent_upgrade"]
+            total_stat_buffs = i["total_stat_buffs"]
+            embed = hikari.Embed(title=operator)
+            embed.add_field("Module Branch", module_branch)
+            embed.add_field("Stage 1 | Trait Upgrade", stage_1_trait_upgrade)
+            if base_talent != "N/A":
+                embed.add_field("Base Talent", base_talent)
+                embed.add_field("Stage 2 | Talent Upgrade", stage_2_talent_upgrade)
+                embed.add_field("Stage 3 | Talent Upgrade", stage_3_talent_upgrade)
             else:
-                await ctx.respond(f"{operator.title()} is not an operator", flags=hikari.MessageFlag.EPHEMERAL)
-        embeds = []
-        for i in data:
-            name = i["operator"]
-            for key, value in ops_id_data.items():
-                if key == name:
-                    operator_code = value
-                    operator_icon = f"https://raw.githubusercontent.com/Aceship/Arknight-Images/main/avatars/{operator_code}.png"
-            if name.lower() == operator.lower():
-                module_branch = i["module_branch"]
-                stage_1_trait_upgrade = i["stage_1_trait_upgrade"]
-                base_talent = i["base_talent"]
-                stage_2_talent_upgrade = i["stage_2_talent_upgrade"]
-                stage_3_talent_upgrade = i["stage_3_talent_upgrade"]
-                total_stat_buffs = i["total_stat_buffs"]
-                embed = hikari.Embed(title=name)
-                embed.add_field("Module Branch", module_branch)
-                embed.add_field("Stage 1 | Trait Upgrade",
-                                stage_1_trait_upgrade)
-                if base_talent != "N/A":
-                    embed.add_field("Base Talent", base_talent)
-                    embed.add_field("Stage 2 | Talent Upgrade",
-                                    stage_2_talent_upgrade)
-                    embed.add_field("Stage 3 | Talent Upgrade",
-                                    stage_3_talent_upgrade)
-                elif base_talent == "N/A":
-                    embed.add_field("Stage 2 | New Talent",
-                                    stage_2_talent_upgrade)
-                    embed.add_field("Stage 3 | Talent Upgrade",
-                                    stage_3_talent_upgrade)
-                embed.add_field("Total Stat Buffs", total_stat_buffs)
-                embed.add_field("Modules Sheet", "https://bit.ly/AKModules")
-                embed.set_thumbnail(operator_icon)
-                if module_branch == "SPC-X":
-                    embed.add_field("Increased Attack Range:", "See below")
-                    embed.set_image("https://i.imgur.com/aQUIiMu.png")
-                if module_branch == "RIN-X":
-                    embed.add_field("Increased Attack Range:", "See below")
-                    embed.set_image("https://i.imgur.com/x8bMsT8.png")
-                if name == "Tomimi":
-                    embed.add_field(
-                        "Slightly Reduced Attack Range:", "See below")
-                    embed.set_image("https://i.imgur.com/dx7Qy8b.png")
-                embeds.append(embed)
+                embed.add_field("Stage 2 | New Talent", stage_2_talent_upgrade)
+                embed.add_field("Stage 3 | Talent Upgrade", stage_3_talent_upgrade)
+            embed.add_field("Total Stat Buffs", total_stat_buffs)
+            embed.add_field("Modules Sheet", "https://bit.ly/AKModules")
+            embed.set_thumbnail(avatar_url)
+            if module_branch == "SPC-X":
+                embed.add_field("Increased Attack Range:", "See below")
+                embed.set_image("https://i.imgur.com/aQUIiMu.png")
+            if module_branch == "RIN-X":
+                embed.add_field("Increased Attack Range:", "See below")
+                embed.set_image("https://i.imgur.com/x8bMsT8.png")
+            if operator == "Tomimi":
+                embed.add_field("Slightly Reduced Attack Range:", "See below")
+                embed.set_image("https://i.imgur.com/dx7Qy8b.png")
+            embeds.append(embed)
     await ctx.respond(embeds=embeds)
 
 
@@ -81,16 +86,9 @@ async def module(ctx):
 async def module_autocomplete(
     opt: hikari.AutocompleteInteractionOption, inter: hikari.AutocompleteInteraction
 ) -> Union[str, Sequence[str], hikari.CommandChoice, Sequence[hikari.CommandChoice]]:
-    user_input = opt.value
-    operators_list = []
-    with open("./data/operator_name_to_id.json", "r") as d:
-        ops = json.load(d)
-        for key, _ in ops.items():
-            operator_name = key
-            operators_list.append(operator_name.lower())
-    close_matches = get_close_matches(user_input, operators_list, cutoff=0.3)
+    user_input = opt.value    
+    close_matches = get_close_matches(user_input, operators_with_modules, cutoff=0.3)
     return close_matches
-
 
 def load(bot):
     bot.add_plugin(plugin)
