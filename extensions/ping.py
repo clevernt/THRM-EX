81b2254b73631963c85f3c394e2d968197df31f0
neuvium <azozy2011.hd@gmail.com>
2023-12-25 12:54:53 +0300

    guilds count

--- extensions/ping.py
+++ extensions/ping.py
@@ -1,14 +1,19 @@
 import lightbulb
 import hikari
 
-plugin = lightbulb.Plugin('ping')
+plugin = lightbulb.Plugin("ping")
+
 
 @plugin.command
-@lightbulb.command('ping', "Check bot's ping")
+@lightbulb.command("ping", "Check bot's ping")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def ping(ctx):
-    embed = hikari.Embed(title="Pong!", description=f"**{plugin.bot.heartbeat_latency * 1_000:.0f}ms**")
+    embed = hikari.Embed(
+        title="Pong!", description=f"**{plugin.bot.heartbeat_latency * 1_000:.0f}ms**"
+    )
+    embed.add_field("Currently in", f"{plugin.bot.cache.get_guilds_view()} guilds!")
     await ctx.respond(embed)
-    
+
+
 def load(bot):
     bot.add_plugin(plugin)
