77d8ca721dbf14e9e7aafb353caff73d5976ff1b
neuvium <azozy2011.hd@gmail.com>
2023-12-25 12:56:28 +0300

    d

--- extensions/ping.py
+++ extensions/ping.py
@@ -11,7 +11,6 @@ async def ping(ctx):
     embed = hikari.Embed(
         title="Pong!", description=f"**{plugin.bot.heartbeat_latency * 1_000:.0f}ms**"
     )
-    embed.add_field("Currently in", f"{plugin.bot.cache.get_guilds_view()} guilds!")
     await ctx.respond(embed)
 
 
