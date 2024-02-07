6164bb320323530df4ba0b1b0de5864d82e3d594
neuvium <azozy2011.hd@gmail.com>
2024-02-07 14:17:09 +0300

    Fix bug when >2* operator is listed without a skill

--- utils/operators_list.py
+++ utils/operators_list.py
@@ -20,8 +20,10 @@ def filter_and_translate(operators_list, operator_names):
         if operator in unskilled_operators:
             operators_list_en.append(operator_names[operator])
         else:
-            skill = operator[-1]
+            skill = f"S{operator[-1]}" if operator[-1].isnumeric() else None
             name_cn = operator.rstrip(skill)
             name_en = operator_names.get(name_cn)
-            operators_list_en.append(f"{name_en} S{skill}")
+            operators_list_en.append(
+                f"{name_en} {skill}" if skill is not None else f"{name_en}"
+            )
     return operators_list_en
