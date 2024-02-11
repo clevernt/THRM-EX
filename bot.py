99449de25c484583e952b9f6fc4e2bbb4e6c75dd
selvuen <azozy2011.hd@gmail.com>
2024-02-11 18:22:54 +0300

    t

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 import lightbulb
+import hikari
 import os
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-bot = lightbulb.BotApp(token=os.getenv("TOKEN"), help_slash_command=True)
+bot = lightbulb.BotApp(token=os.getenv("TEST_TOKEN"), help_slash_command=True)
 
 bot.load_extensions_from("extensions")
 
 bot.run()
+print(bot.cache.get_guilds_view())
