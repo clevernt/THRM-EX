d648f1aca809643f275f9fd345420bf27de449d0
selvuen <azozy2011.hd@gmail.com>
2024-02-11 18:24:19 +0300

    oops !

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-bot = lightbulb.BotApp(token=os.getenv("TEST_TOKEN"), help_slash_command=True)
+bot = lightbulb.BotApp(token=os.getenv("TOKEN"), help_slash_command=True)
 
 bot.load_extensions_from("extensions")
 
