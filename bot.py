5174482d514f2d6e4d12354c5af4eab0241cc60b
selvuen <azozy2011.hd@gmail.com>
2024-02-11 18:48:29 +0300

    testing

--- bot.py
+++ bot.py
@@ -5,7 +5,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-bot = lightbulb.BotApp(token=os.getenv("TOKEN"), help_slash_command=True)
+bot = lightbulb.BotApp(
+    token=os.getenv("TOKEN"),
+    help_slash_command=True,
+    default_enabled_guilds=753858152486797352,
+)
 
 bot.load_extensions_from("extensions")
 
