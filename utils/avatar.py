f8a149d357eb21430bd7666d3cf15a842218ec28
selvuen <azozy2011.hd@gmail.com>
2024-03-31 09:07:52 +0300

    Use MayerBotPics repo for operator avatars

--- utils/avatar.py
+++ utils/avatar.py
@@ -1,27 +1,12 @@
-import requests
 import json
 
-from bs4 import BeautifulSoup
-
+url = "https://raw.githubusercontent.com/KrisTheNewest/MayerBotPics/master/avatars"
 
 def get_avatar(operator):
-
     with open("./data/operators.json", "r", encoding="utf-8") as f:
         data = json.load(f)
         for operator_name, operator_data in data.items():
             if operator_name.lower() == operator.lower():
-                chinese_name = operator_data["name"]
+                operator_id = operator_data["id"]
 
-    link = f"https://prts.wiki/w/文件:头像_{chinese_name}.png"
-    response = requests.get(link)
-
-    if response.status_code == 200:
-        html_content = response.text
-        soup = BeautifulSoup(html_content, "html.parser")
-
-        img_link = soup.select_one(".fullImageLink a")["href"]
-
-        return img_link
-
-    else:
-        return None
+    return f"{url}/{operator_id}.webp"
