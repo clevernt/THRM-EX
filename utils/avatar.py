3a30a489137887f8df7a527a0f7a907c1fc4531a
selvuen <azozy2011.hd@gmail.com>
2024-04-05 03:39:39 +0300

    FEAT: Switch to an auto-updated repo for operator avatars

--- utils/avatar.py
+++ utils/avatar.py
@@ -1,6 +1,7 @@
 import json
 
-url = "https://raw.githubusercontent.com/KrisTheNewest/MayerBotPics/master/avatars"
+url = "https://raw.githubusercontent.com/yuanyan3060/ArknightsGameResource/main/avatar"
+
 
 def get_avatar(operator):
     with open("./data/operators.json", "r", encoding="utf-8") as f:
@@ -9,4 +10,4 @@ def get_avatar(operator):
             if operator_name.lower() == operator.lower():
                 operator_id = operator_data["id"]
 
-    return f"{url}/{operator_id}.webp"
+    return f"{url}/{operator_id}_2.png"
